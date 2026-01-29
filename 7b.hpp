c37b4c83663150f27cbf14cb0d12fd9d2262fce7
starssxhfdmh <starssxproject@gmail.com>
2026-01-29 16:43:30 +0700

    Bump version to 2.3.0

--- 7b.hpp
+++ 7b.hpp
@@ -2,7 +2,7 @@
 /// @brief A single-header C++ build system.
 /// @author starssxhfdmh
 /// @copyright Copyright (c) 2026 starssxhfdmh. MIT License.
-/// @version 2.2.0
+/// @version 2.3.0
 ///
 /// @details
 /// 7b is a lightweight, header-only build system written in C++17.
